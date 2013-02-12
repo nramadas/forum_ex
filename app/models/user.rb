8cd216f6da3af7fbb89ae757a850ce0529415802
Niranjan <n.r.b.ramadas@gmail.com>
2013-02-11 21:21:23 -0800

    Update app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :last_viewed_user_page
   validates :name, :length => { in: 4..16 }, uniqueness: true
+  
+  # one thing I like to do (that you might like as well) is use
+  # validates_uniqueness_of and
+  # validates_length_of to do my validations. It may be more
+  # lines of code, but it is more explicit
 
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
